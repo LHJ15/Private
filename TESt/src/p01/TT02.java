0a6ab507e959b19ae4f4ace88167aa5be351ce0c
이헌지 <hw2163@naver.com>
2023-06-13 15:35:02 +0900

    .

--- TESt/src/p01/TT02.java
+++ TESt/src/p01/TT02.java
@@ -13,23 +13,21 @@ class Buyer{
 	public void Payment(Seller seller,int money )
 	{
 		
-		MyMoney-=money;
+		MyMoney-=money;     //마이머니에서 머니만큼 차감
 		
-		int cnt= seller.Receive(money);
+		int cnt= seller.Receive(money);          //셀러에게 돈 전달   //셀러러부터 사과갯수받기!!
 	
-		AppleCnt+=cnt;
+		AppleCnt+=cnt;                  //사과개수 Cnt누적
 	}
-	public void ShowInfo() {
+	
+	public void ShowInfo() {                                 //속성 정보 Showinfo에출력
 		
-		System.out.println("-----구매자정보------");
+	
 		System.out.println("보유 금액 : "+ MyMoney);
-		System.out.println("사과 개수 : "+ AppleCnt);
+		System.out.println("사과 개수 : "+ AppleCnt);     
 	}
 }
-	
-	
-
-
+ 
 
 class Seller{
 	private int MyMoney;
@@ -37,30 +35,30 @@ class Seller{
 	public int Price;
 	
 	public Seller(int myMoney, int appleCnt, int price) {
-		MyMoney = myMoney;
+		MyMoney = myMoney;        
 		AppleCnt = appleCnt;
 		Price = price;
 	}
-	public int Receive(int money) {
+	public int Receive(int money) {          //머니누에적
 		
-		MyMoney+=money;
+		MyMoney+=money;                       //Mymomey에 money 누적
 		
-		int revcnt=money/Price;
-		AppleCnt-=revcnt;
-		return revcnt;
+		int revcnt=money/Price;               //money에 해당하는 만큼 사과개수를 계산한다  
+		
+		AppleCnt-=revcnt;                      // AppleCnt에서 계산된 만큼의 사과개수를 차감
+		
+		return revcnt;                        //계산된 사과개수리턴
+		                                    
 	}
 	public void ShowInfo() {
 	
-		System.out.println("-----판매자정보-----");
+		System.out.println("-----판매자 정보-----");
 		System.out.println("보유 금액 : "+ MyMoney);
 		System.out.println("사과 개수 : "+ AppleCnt);
 		System.out.println("개당 가격 : "+ Price);
 
 	}
 }
-	
-	
-	
 	
 
 public class TT02 {
@@ -69,20 +67,27 @@ public class TT02 {
 	public static void main(String[] args) {
 		
 		
-				Seller 하이브1 = new Seller(100000,100,1000);//보유금액,사과개수,개당가격
-				Buyer 아이브 = new Buyer(10000,0);	//보유금액,사과개수
-				Buyer 르세라핌 = new Buyer(20000,0);
-				Buyer 뉴진스 = new Buyer(30000,0);
-				
+				Seller 방시혁 = new Seller(100000,100,1000);     //보유금액,사과개수,개당가격
+				Buyer BTS = new Buyer(10000,0);	        
+				Buyer 르세라핌 = new Buyer(20000,0);        //보유금액,사과개수
+				Buyer 뉴진스 = new Buyer(30000,0);				
 		
-		        아이브.Payment(하이브1,2000);
-		        르세라핌.Payment(하이브1,5000);
-		        뉴진스.Payment(하이브1, 3000);
-		        
-		        하이브1.ShowInfo();
-		        아이브.ShowInfo();
+		        BTS.Payment(방시혁,2000);
+		        르세라핌.Payment(방시혁,5000);
+		        뉴진스.Payment(방시혁, 3000);		               //돈지불한만 큼 사과 Get
+		   
+		        방시혁.ShowInfo();
+		    	System.out.println("-----구매자 정보------");     //출력
+		    	System.out.println("-BTS-");
+		        BTS.ShowInfo();	
+		    	System.out.println("-르세라핌-");
 		        르세라핌.ShowInfo();
+		    	System.out.println("-뉴진스-");
 		        뉴진스.ShowInfo();
+		   
+		       //1
+		        
+		        
 		        
 		        
 			    
@@ -97,5 +102,8 @@ public class TT02 {
 	}
 
 }
+  
+  
+
   
   
